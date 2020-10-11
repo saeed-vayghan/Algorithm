9eb656817e537a09e048c6151530a9d443be53f6
Saeed Vayghani <saeed.uni68@gmail.com>
2020-10-12 00:38:41 +0330

    Create find-sets-of-numbers-adds-up-to-x.js

---
+++ find-sets-of-numbers-adds-up-to-x.js
@@ -0,0 +1,50 @@
+/*
+
+Dynamic Programming, Find Sets Of Numbers That Add Up To X
+
+Sample: arr = [2, 5, 7, 12], X = 19
+Sets: {7, 12} / {2, 5, 12}
+
+*/
+
+
+const findSets = (arr, x, i, memo) => {
+  const memoKey = `${x}:${i}`;
+
+  if ( memo[key] !=== undefined ) {
+    return memo[key];
+  }
+
+  if ( x === 0 ) {
+    return 1;
+  }
+
+  if ( x < 0 ) {
+    return 0;
+  }
+
+  if ( i < 0 ) {
+    return 0;
+  }
+
+
+  let returnVal = 0;
+
+  if ( x < arr[i] ) {
+    returnVal = recursice(arr, x, i-1);
+
+  } else {
+
+    returnVal = recursice(arr, x, i-1) + recursice(arr, x - arr[i], i-1);
+  }
+
+  memo[key] = returnVal;
+
+  return returnVal;
+}
+
+const findSets = (arr, x) => {
+  const memo = {};
+
+  return recursice(arr, x, arr.length - 1, memo);
+}
